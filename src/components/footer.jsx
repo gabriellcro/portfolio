1026ebaf880b1eb17c49bcf7ead33fde17b77c41
Gabriel <gabriellcro@proton.me>
2025-04-19 12:46:00 -0300

    style: tooltip removido

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,5 @@
-import { useIsMobile } from "@/hooks/useMobile";
 import { Button } from "./ui/button";
 import { Github, Linkedin, Instagram, Twitter } from "lucide-react";
-
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 
 const data = [
   {
@@ -33,33 +25,15 @@ const data = [
 ];
 
 export default function Footer() {
-  const isMobile = useIsMobile();
   return (
     <footer className="flex items-center justify-center w-full p-5 text-primary">
-      {isMobile
-        ? data.map((item, idx) => (
-            <Button key={idx} variant="ghost" size="icon">
-              <a href={item.link} target="_blank">
-                {item.icon}
-              </a>
-            </Button>
-          ))
-        : data.map((item, idx) => (
-            <TooltipProvider key={idx}>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Button variant="ghost" size="icon">
-                    <a href={item.link} target="_blank">
-                      {item.icon}
-                    </a>
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>{item.title}</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          ))}
+      {data.map((item, idx) => (
+        <Button key={idx} variant="ghost" size="icon" asChild>
+          <a href={item.link} target="_blank">
+            {item.icon}
+          </a>
+        </Button>
+      ))}
     </footer>
   );
 }
